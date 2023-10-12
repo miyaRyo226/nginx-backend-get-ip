82d3d43b8e45f21b9de2ebaf16f71be45da3b815
ryoya <ryoya.miyamoto226@gmail.com>
2023-10-12 22:10:56 +0900

    fix:

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Request() req:Request): string {
+    console.log(req.headers)
     return this.appService.getHello();
   }
 }
